e19924399bb2fc3184d61a0b1c2453daeaf3933c
Кирилл Козлов <kirill_kozlov_1@mail.ru>
2019-03-18 21:47:14 +0300

    hm04-2

---
+++ hm04-1/hm04-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите W A S D");
+            String choise = Console.ReadLine();
+                           
+            switch (choise)
+            {
+                case "w":
+                    Console.WriteLine("Вверх");
+                    break;
+                case "a":
+                    Console.WriteLine("Назад");
+                    break;
+                case "s":
+                    Console.WriteLine("Вниз");
+                    break;
+                case "d":
+                    Console.WriteLine("Вниз");
+                    break;
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
