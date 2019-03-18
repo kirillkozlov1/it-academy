12a0cf66896c05f266d864fdb70081af3081e844
Кирилл Козлов <kirill_kozlov_1@mail.ru>
2019-03-18 21:25:08 +0300

    hm03-3

---
+++ hm03-1/hm03-3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm03_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            string num1 = Console.ReadLine();
+            int a = Convert.ToInt32(num1);
+            Console.WriteLine("Введите второее число");
+            string num2 = Console.ReadLine();
+            int b = Convert.ToInt32(num2);
+            Console.WriteLine("Введите сумму двух чисел");
+            string res = Console.ReadLine();
+            int c = Convert.ToInt32(res);
+            if (c == a + b)
+                Console.WriteLine("Правильно");
+            else
+                Console.WriteLine("Не правильно");
+            if (c < a + b)
+                Console.WriteLine("Должно быть больше");
+
+            if (c > a + b)
+                Console.WriteLine("Должно быть меньше");
+
+            Console.ReadKey();
+        }
+    }
+}
