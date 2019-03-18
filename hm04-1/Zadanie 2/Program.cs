03cc3476fdfa3d2048d511f7891e075d1598ff36
Кирилл Козлов <kirill_kozlov_1@mail.ru>
2019-03-18 21:54:55 +0300

    Задание на подсчет букв А

---
+++ hm04-1/Zadanie 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово на русском языке");
+            string word = Console.ReadLine();
+            int schet = 0;
+            char poisk = 'а';
+
+            foreach (Char letter in word)
+                if (letter == poisk) schet++;
+            {
+                Console.WriteLine("Количество букв а:" + schet);
+            }
+            Console.ReadKey();
+        }
+    }
+}
