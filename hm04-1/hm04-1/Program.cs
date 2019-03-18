7d4ad033de8ba1c221ff86826ea298fe8bceac54
Кирилл Козлов <kirill_kozlov_1@mail.ru>
2019-03-18 21:33:30 +0300

    hm04-1

---
+++ hm04-1/hm04-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char c = 'Z'; c >= 'A'; c--)
+                Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
